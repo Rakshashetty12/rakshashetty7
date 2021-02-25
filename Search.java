29ffb96e16d35df32f682eaa78eaaae83273c810
Rakshashetty12 <78653996+Rakshashetty12@users.noreply.github.com>
2021-02-25 21:58:02 +0530

    search the number
    search

--- Search.java
+++ Search.java
@@ -1,7 +1,7 @@
 class Search
 	{
 	public static void main(String args[]){
-		int num[]={6,8,3,7,5,6,1,4};		
+		int num[]={6,8,3,7,9,6,2,4};		
 		int val=5;
 		boolean found=false;
 		for(int val : num){                  
